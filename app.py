015e542e25d1d4ee482c906d107a2ddca508973a
Glebuar <glebuar@gmail.com>
2024-04-07 12:56:32 -0700

    changed image format to binary

--- app.py
+++ app.py
@@ -1,40 +1,31 @@
 from flask import Flask, request, jsonify
-import base64
-from io import BytesIO
 from PIL import Image
 from api.image_processor import get_secret, get_possible_message, upload_to_gcs
 from api.text_to_speech import tts_openai
 import os
 
 app = Flask(__name__)
+messages_history = []
 
 @app.route('/upload', methods=['POST'])
 def upload_image():
-    if 'image' in request.json:
-        image_data = request.json['image']
-        try:
-            decoded_image = base64.b64decode(image_data)
-            image = Image.open(BytesIO(decoded_image))
-            
+    if 'image' in request.files:
+        image_file = request.files['image']
+        try:            
+            image = Image.open(image_file)            
             temp_image_path = f'temp_image.{image.format.lower()}'
-            image.save(temp_image_path)
-            
+            image.save(temp_image_path)            
             api_key = get_secret()
-            bucket_name = "images_from_client"
-           
+            bucket_name = "images_from_client"            
             url = upload_to_gcs(bucket_name, temp_image_path)
-            
-            messages = []
-            message = get_possible_message(url, messages, api_key)
-            
+            global messages_history
+            message = get_possible_message(url, messages_history, api_key)
+            messages_history.append(message)            
             if message == "NOTHING":
                 os.remove(temp_image_path)
-                return jsonify({'tts_response': ''})
-            
-            tts_response = tts_openai(message, api_key, bucket_name)
-            
-            os.remove(temp_image_path)
-            
+                return jsonify({'tts_response': ''})            
+            tts_response = tts_openai(message, api_key, bucket_name)            
+            os.remove(temp_image_path)            
             return jsonify({'tts_response': tts_response})
         except Exception as e:
             return jsonify({'error': str(e)}), 400
